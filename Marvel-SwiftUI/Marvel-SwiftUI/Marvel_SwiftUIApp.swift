95a9869fdf9dfb3ea5c5e9d00764a726446a8618
Guilherme Silva <guilhermesilva@Guilhermes-MacBook-Pro.local>
2024-01-23 15:15:12 -0300

    service api call structure

---
+++ Marvel-SwiftUI/Marvel-SwiftUI/Marvel_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_SwiftUIApp.swift
+//  Marvel-SwiftUI
+//
+//  Created by Guilherme Silva on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
